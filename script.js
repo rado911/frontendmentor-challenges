95ac7f9e97ca6fa6dc4fc5f2f4cbb136bad33a5b
rado911 <rado.wilczek@gmail.com>
2022-02-13 22:29:46 +0100

    basic functionality implemented, edited styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".percent");
 const amount = document.querySelector(".dollar");
+const people = document.querySelector(".people");
 
 let billAmount;
 let percents;
@@ -10,8 +11,21 @@ buttons.forEach((button) => {
     buttons.forEach((btn) => {
       btn.classList.contains("clicked") ? btn.classList.remove("clicked") : "";
     });
+    //Switching button color
     button.classList.toggle("clicked");
+    //Saving bill, % a amount of people
     billAmount = amount.value;
-    console.log(billAmount);
+    percents = Number(button.value);
+    persons = Number(people.value);
+    console.log(billAmount, percents);
+    displayEnd(billAmount, percents, persons);
   });
 });
+console.log(percents);
+function displayEnd(bill, perc, pers = 0) {
+  const amount = bill * perc;
+  const person = amount + bill / pers;
+  console.log(person);
+  document.getElementById("tip-amount").innerHTML = `$${amount}`;
+  document.getElementById("person-amount").innerHTML = `$${person}`;
+}
