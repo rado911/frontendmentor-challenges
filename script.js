aff96ed42cf57a7482676635f2e316b1913c1910
rado911 <rado.wilczek@gmail.com>
2022-02-17 14:56:07 +0100

    styling fixed, updated input validation and reset button works

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const buttons = document.querySelectorAll(".percent");
+const manual = document.getElementById("manual");
 const amount = document.querySelector(".dollar");
 const people = document.querySelector(".people");
+const warning = document.querySelector(".warning");
+const tip = document.getElementById("tip-amount");
+const perpers = document.getElementById("person-amount");
+const reset = document.querySelector(".reset");
 
 let billAmount;
 let percents;
@@ -18,14 +23,27 @@ buttons.forEach((button) => {
     percents = Number(button.value);
     persons = Number(people.value);
     console.log(billAmount, percents);
-    displayEnd(billAmount, percents, persons);
+    //CHECKING FOR AMOUNT OF PEOPLE IS NOT 0
+    if (people.value === "" || people.value === "0") {
+      warning.classList.add("error");
+    } else {
+      warning.classList.remove("error"),
+        displayEnd(billAmount, percents, persons);
+    }
   });
+
+  console.log(percents);
+  function displayEnd(bill, perc, pers = 0) {
+    const amount = bill * perc;
+    const person = amount + bill / pers;
+    console.log(person);
+    document.getElementById("tip-amount").innerHTML = `$${amount.toFixed(2)}`;
+    document.getElementById("person-amount").innerHTML = `$${person}`;
+  }
 });
-console.log(percents);
-function displayEnd(bill, perc, pers = 0) {
-  const amount = bill * perc;
-  const person = amount + bill / pers;
-  console.log(person);
-  document.getElementById("tip-amount").innerHTML = `$${amount}`;
-  document.getElementById("person-amount").innerHTML = `$${person}`;
-}
+
+//RESETING APP
+reset.addEventListener("click", () => {
+  amount.value = people.value = "";
+  tip.textContent = perpers.textContent = "$0.00";
+});
